bb99d9df27a1d85b332ff99cb60ac042e5568bb2
AndyJmnz <a17100657@gmail.com>
2024-03-01 13:16:00 -0600

    Agregar Autentificacion, proteger rutas y Ocultar Password

--- src/routes/task.route.ts
+++ src/routes/task.route.ts
@@ -3,10 +3,16 @@ import { Tasks } from '../types/task.type'
 
 import TaskService from '../services/task.service'
 
+import passport from 'passport'
+import { UserRequestType } from '../types/user.type'
+
 const router = express.Router()
 const service = new TaskService()
 
-router.post('/', async (req, res) => {
+router.post(
+  '/', 
+  passport.authenticate('jwt',{ session:false}),
+  async (req, res) => {
     try {
         const taskData: Tasks = req.body;
         const newTask = await service.create(taskData)
@@ -16,33 +22,46 @@ router.post('/', async (req, res) => {
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
-})
+  }
+)
 
-router.get('/', async (req, res, next) => {
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }), 
+  async (req, res, next) => {
     try {
         const tasks = await service.findAll()
         res.status(200).json(tasks)
     } catch (error) {
         next(error)
     }
-})
+  }
+)
 
-router.get('/findById/:id', async (req, res, next) => {
+router.get(
+  '/findById/:id', 
+  passport.authenticate('jwt', { session: false }),
+  async (req, res, next) => {
     try {
       const task = await service.findById(req.params.id)
       res.status(200).json(task)
     } catch (error) {
       next(error)
     }
-})
+  }
+)
   
-router.get('/findByName', async (req, res, next) => {
+router.get(
+  '/findByName', 
+  passport.authenticate('jwt', { session: false }),
+  async (req, res, next) => {
     try {
       const task = await service.findByName(req.query.name as string)
       res.status(200).json(task)
     } catch (error) {
       next(error)
     }
-})
+  }
+)
 
 export default router
